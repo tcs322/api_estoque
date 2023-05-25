a7dd19bb350b88b743ffce4ba203abc3dedee4e2
Thyerre Sampaio <thyerre322@gmail.com>
2023-05-25 14:40:20 -0300

    adicionando rota principal com arquivo index.ts e testando primeira execução

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log("Thyerre");
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const PORT = 3500;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
